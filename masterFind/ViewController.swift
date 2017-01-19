8b2d81a9797ee12ab3fd0390373c92cb684474fb
yokoyama.toshiyuki <yokoyama.toshiyuki@N517.local>
2017-01-19 11:42:43 +0900

    Initial Commit

---
+++ masterFind/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  masterFind
+//
+//  Created by yokoyama.toshiyuki on 2017/01/19.
+//  Copyright © 2017年 yokoyama.toshiyuki. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
